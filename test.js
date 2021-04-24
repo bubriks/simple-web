3f40a9f7f40153a66d63ce0c696accc6462b86f5
Agriad <agriad1@yahoo.com>
2021-04-24 14:27:25 +0200

    Add easter egg

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const assert = require("assert");
 
-it("should perfrom string concatenation", function() {
-    assert.strictEqual("a" + "b", "ab");
+it("should perform string concatenation", function() {
+    assert.strictEqual("haha, " + "made you look", "haha, made you look");
 });
 
 it("should perform arithmetic addition", () => {
-    assert.strictEqual(2 + 2, 4);
+    assert.strictEqual(1241 + 1241, 2482);
 });
